af2daec96510c0e76ba8ea6a05797a3137336801
Onintze <Onintze@2SMA1.mshome.net>
2020-11-18 13:52:39 +0100

    menú 4 opciones

---
+++ EndeGit/src/menuak.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class menuak {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int elige = 0;
+		
+		do {
+			System.outsds
+			elige = Integer.parseInt(sc.nextLine());
+			switch(elige) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			}
+		}
+		while(elige!=0);
+		
+	}
+
+}
